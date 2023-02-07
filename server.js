91ecb5c1ec5911a4c790e64392612df938c324a7
Rafael Andrade <andrade.inf@gmail.com>
2023-02-07 19:30:33 +0000

    changing cloud build to use yaml configuration

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log('New Connection');
-    res.end('Hello World!');
+    res.end('Hello World - with yaml file!');
 });
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log('Listening...'));
