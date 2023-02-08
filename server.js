2acc47422784cfcc3ad2dedf83bcc5c87643f011
Rafael Andrade <andrade.inf@gmail.com>
2023-02-08 15:59:38 +0000

    final version for basic implementation

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log('New Connection');
-    res.end('Hello World - with yaml file and cloud build and forced change!');
+    res.end('Hello World - Final!');
 });
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log('Listening...'));
